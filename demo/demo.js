15d3d116f60d1a6157cd97a5d0655e48d196a620
Winme <huangweimin@zto.com>
2020-03-14 00:57:50 +0800

    publish  version 1.1.3

--- demo/demo.js
+++
@@ -1,9 +0,0 @@
-const { ReactPopover: Popover, React, ReactDOM } = window
-
-function App() {
-  <Popover content="这是一段文本信息">
-    <span>点击我</span>
-  </Popover>
-}
-
-ReactDOM.render(<App/>, document.getElementById('container'))
